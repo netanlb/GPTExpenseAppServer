ba8ea6e5c69b201c749763962784cb698c0a431a
netanlb <netanlb@gmail.com>
2023-07-08 19:02:16 +0300

    removed unnecessary stuff

--- models/ComputedCosts.js
+++ models/ComputedCosts.js
@@ -1,62 +1,40 @@
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose');
-
-//this is a single object that goes inside the categories lists in the computedCostsSchema (see below)
+// This is a single object that goes inside the categories list in the computedCostsSchema
 const CostInfoComputedSchema = new mongoose.Schema({
-    day:{
-        type: Number,
-        required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    sum: {
-      type: Number,
-      required: true,
-    },
+  category: {
+    type: String,
+    required: true,
+  },
+  sum: {
+    type: Number,
+    required: true,
+  },
+});
 
-  });
-
-//this is this computed costs of all the costs (group by category) for a single month in a year (for a single user_id)
-//saved in the computedcosts collection in db (the report is reading this object)
+// This is the computed costs of all the costs (group by category) for a single month in a year (for a single user_id)
+// saved in the computedcosts collection in db (the report is reading this object)
 const computedCostsSchema = new mongoose.Schema({
-    header:{
-        type: String,
-        required: true,
-    },
-    food: {
-      type: [CostInfoComputedSchema],
-      required: true,
-    },
-    health: {
-      type: [CostInfoComputedSchema],
-      required: true,
-    },
-    housing: {
-      type: [CostInfoComputedSchema],
-      required: true,
-    },
-    sport: {
-      type: [CostInfoComputedSchema],
-      required: true,
-    },
-    education: {
-      type: [CostInfoComputedSchema],
-      required: true,
-    },
-    transportation: {
-      type: [CostInfoComputedSchema],
-      required: true,
-    },
-    other: {
-      type: [CostInfoComputedSchema],
-      required: true,
-    },
-  });
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  month: {
+    type: Number,
+    required: true,
+  },
+  groupedCategory: [CostInfoComputedSchema], // This is an array of CostInfoComputed documents
+});
 
-  //this is the only way I managed to export both of the models (create 2 consts and exporting them)
-  const computedCostsModel =  mongoose.model('ComputedCosts', computedCostsSchema);
-  const costInfoComputedModel = mongoose.model('CostInfoComputed', CostInfoComputedSchema);
+// Create the models
+const computedCostsModel = mongoose.model("ComputedCosts", computedCostsSchema);
+const costInfoComputedModel = mongoose.model(
+  "CostInfoComputed",
+  CostInfoComputedSchema
+);
 
-module.exports =  {computedCostsModel, costInfoComputedModel};
+module.exports = { computedCostsModel, costInfoComputedModel };
--- routes/cost.js
+++ routes/cost.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Costs = require("../models/Costs");
-const {
-  computedCostsModel,
-  costInfoComputedModel,
-} = require("../models/ComputedCosts");
 
 // Update a cost by ID
 router.put("/:id", async (req, res) => {
@@ -145,13 +141,6 @@ router.get("/", async (req, res) => {
   }
 });
 
-//addcost post
-//summery - first adding the cost to the Costs collection
-//second we create a "CostInfoComputed" instance that represent our new cost (this instance goes into the categories lists)
-//then according to the computed pattern, need to store the cost into "ComputedCosts"
-//document (in the respective collection) - so check if the user_id,month and year have a ComputedCosts on the db,
-//if not - create one with the appropriate CostInfoComputed inside the needed array and save it to db,
-//if the document exist, then just update the "CostInfoComputed" object to the needed array inside
 router.post("/", async (req, res) => {
   try {
     checkAddcostSchema(req.body);
@@ -160,12 +149,6 @@ router.post("/", async (req, res) => {
     const datestring = req.body.date;
 
     const date = new Date(datestring);
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-
-    //the ComputedCosts header (the identifier)
-    const computedHeader = `${user_id}-${year}-${month}`;
 
     const cost_instance = new Costs({
       user_id,
@@ -177,89 +160,13 @@ router.post("/", async (req, res) => {
 
     //save to db the cost instance
     const savedCost = await cost_instance.save();
-
     console.log("cost_instance.save()");
-
-    // //need the cost info item for the computed costs
-    // const costItemForComputedCosts = new costInfoComputedModel({
-    //   day: day,
-    //   description: description,
-    //   sum: sum,
-    // });
-
-    // //check if the computed costs object is already in db
-    // const computedCostsFromDB = await computedCostsModel.findOne({
-    //   header: computedHeader,
-    // });
-
-    // console.log("ComputedCosts.findOne({header: computedHeader}");
-
-    // if (computedCostsFromDB == null) {
-    //   //need to add to db a new ComputedCosts for the given user_id, month and year
-    //   //because there is no document like it in db
-
-    //   console.log("if if if");
-
-    //   const newComputedCosts = getNewComputedCosts(
-    //     computedHeader,
-    //     category,
-    //     costItemForComputedCosts
-    //   );
-
-    //   await newComputedCosts.save();
-
-    //   console.log("newComputedCosts.save()");
-    // } else {
-    //   console.log("else else");
-    //   //ComputedCosts already in db, so need to update in db, first add the costItemForComputedCosts
-    //   addCostItemToComputedCosts(
-    //     computedCostsFromDB,
-    //     category,
-    //     costItemForComputedCosts
-    //   );
-
-    //   //now update the change
-    //   await computedCostsModel.findOneAndUpdate(
-    //     { header: computedHeader },
-    //     computedCostsFromDB,
-    //     { upsert: true }
-    //   );
-
-    //   console.log("computedCostsFromDB.save()");
-    // }
 
     res.status(200).json(savedCost);
   } catch (err) {
     res.status(400).send(err.message);
   }
 });
-
-// //creates and returns a new empty instance of ComputedCosts and add the CostInfoComputed into the needed array inside
-// function getNewComputedCosts(computedHeader, category, costItem) {
-//   const computedCosts_instance = new computedCostsModel({
-//     header: computedHeader,
-//     food: [],
-//     health: [],
-//     housing: [],
-//     sport: [],
-//     education: [],
-//     transportation: [],
-//     other: [],
-//   });
-
-//   console.log("getNewComputedCosts");
-//   computedCosts_instance[category].push(costItem);
-
-//   console.log("getNewComputedCosts push");
-
-//   return computedCosts_instance;
-// }
-
-// //the ComputedCost is already inside the db, so add to the instance we got from db the CostInfo
-// function addCostItemToComputedCosts(computedCostsFromDB, category, costItem) {
-//   computedCostsFromDB[category].push(costItem);
-//   console.log("addCostItemToComputedCosts");
-// }
 
 //check if we got all the params needed to run all needed stuff
 function checkAddcostSchema(cost) {
